af592e15eb1abbdc403c0f24f761106d3355493b
mdaminulnsl <mdaminul@nextsolutionlab.com>
2023-03-07 12:47:48 +0600

    learning-components app added

---
+++ ReactJS/daily-practice/applications/learning-components/src/Movie.js
@@ -0,0 +1,17 @@
+
+
+const Movie = ({ movie }) => {
+
+    return (
+        <li className="movie" key={movie.id}>
+            <img src={movie.poster} alt={movie.title} />
+            <p>
+                {movie.title} by {movie.director}
+                was relased on {movie.year}
+            </p>
+            <p>Rating: {movie.rating}</p>
+        </li>
+    )
+};
+
+export default Movie;
